e467cdd4dd998aa3fce2d3e0f97596d9dff09da5
Filip <fvanler@gmail.com>
2015-04-11 18:59:40 +0200

    changing forms.py to 'enable' the issue by default

--- issue_397/forms.py
+++ issue_397/forms.py
@@ -15,10 +15,11 @@ class BookFormBase(autocomplete_light.ModelForm):
 
 class BookForm(BookFormBase):
     # if model is NOT specified, then django-autocomplete-light 2.1.0RC2 doesn't work...
-    # try replacing the below with "pass"
-    class Meta:
+    '''class Meta:
         model = models.Book
         fields = ('authors', )
+    '''
+    pass
 
 
 # pip install -e git+https://github.com/yourlabs/django-autocomplete-light.git#egg=autocomplete_light
